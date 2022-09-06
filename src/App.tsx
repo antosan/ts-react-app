c1f5aad564f6bd296b2ea3da6c02c309145001cb
Antony Sande <sandeantony@gmail.com>
2022-09-06 07:28:20 +0300

    Initial JS app

---
+++ src/App.tsx
@@ -0,0 +1,58 @@
+import { useState } from 'react';
+import './App.css';
+import Board from './Board';
+import { calculateWinner } from './utils';
+
+export default function App() {
+  const [history, setHistory] = useState([{ squares: Array(9).fill(null) }]);
+  const [stepNumber, setStepNumber] = useState(0);
+  const [xIsNext, setXIsNext] = useState(true);
+
+  function handleClick(i) {
+    const currentHistory = history.slice(0, stepNumber + 1);
+    const currentStep = currentHistory[currentHistory.length - 1];
+    const squares = currentStep.squares.slice();
+
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
+    squares[i] = xIsNext ? 'X' : 'O';
+
+    setHistory(currentHistory.concat([{ squares }]));
+    setStepNumber(currentHistory.length);
+    setXIsNext(!xIsNext);
+  }
+
+  function jumpTo(step) {
+    setStepNumber(step);
+    setXIsNext(step % 2 === 0);
+  }
+
+  const current = history[stepNumber];
+  const winner = calculateWinner(current.squares);
+
+  const moves = history.map((_step, move) => {
+    const desc = move ? `Go to move #${move}` : 'Go to game start';
+
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{desc}</button>
+      </li>
+    );
+  });
+
+  const status = winner ? `Winner: ${winner}` : `Next player: ${xIsNext ? 'X' : 'O'}`;
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board squares={current.squares} onClick={(i) => handleClick(i)} />
+      </div>
+      <div className="game-info">
+        <div>{status}</div>
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+}
